8e8001d3319332723f6aadefa1e4128dd085bb03
Rachel Katz <rachel.katz@flatironschool.com>
2022-05-03 10:25:01 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+//import react library
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
+//rendering my main component App
 function App() {
+
+  //changes when button is clicked
+  //will run useEffect in DogPics
+  const [runEffect ,setRunEffect] = useState(1);
+
+  //handleClickMe sets runEffect state and increments by 1
+  const handleClickMe = (e) => {
+    //call set runEffect
+    //setRunEffect updates runEffect which is in dependency array
+    //in DogPics at useEffect:15
+    setRunEffect(runEffect => runEffect + 1)
+  }
+
+  //useEffect hook
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
-
-  return <button>Click Me</button>;
+  
+  //JSX to be rendered to the DOM
+  return (<div>
+      <DogPics runEffect={runEffect}/>
+      <button onClick={handleClickMe}>Click Me: {runEffect}</button>
+    </div>);
 }
 
+//export default App to be used in index.js
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -1,22 +1,39 @@
+//import React library and hooks
 import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
-  const [images, setImages] = useState([]);
 
+//DogPics component to be imported in App.js
+function DogPics({runEffect}) {
+  //state for list of dog images
+  const [images, setImages] = useState([]);
+  
+
+  //effect HOOK to fetch GET list of dog images
+  //only runs after DOM is rendered or re-rendered
   useEffect(() => {
+    console.log(runEffect);
     console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
         console.log("setState");
+        //SET STATE - re-render page .... 
         setImages(data.message);
       });
-  });
+  }, [runEffect]);
 
+  //will run every time there is a render or re-render
+  //because there is no dependency array
+  useEffect(() => {
+    alert('hi');
+  })
+
+  //sidenote: this will run once BEFORE use-effect 
   console.log("render");
 
+  //JSX to be rendered and displayed on the DOM
   return (
     <div>
       {images.map((image) => (
@@ -26,4 +43,5 @@ function DogPics() {
   );
 }
 
+//DogPics component to be imported in App.js
 export default DogPics;
